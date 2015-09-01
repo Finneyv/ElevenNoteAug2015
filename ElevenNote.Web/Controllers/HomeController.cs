62fa5d4fc18ed33ab637ed3bbf40c276b71c1405
Vic Finney <finneyv09@gmail.com>
2015-09-01 15:40:22 -0400

    Added guessing game

---
+++ ElevenNote.Web/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Vic()
+        {
+            ViewBag.Message = "Vic";
+
+            return View();
+        }
+
+        public ActionResult Finney()
+        {
+            ViewBag.Message = "Finney";
+            return View();
+        }
+    }
+}
---
+++ ElevenNote.Web/Models/GuessingGameViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElevenNote.Web.Models
+{
+    public class GuessingGameViewModel
+    {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(20)]
+        public String Name { get; set; }
+
+        [Required]
+        [Range(1, 10)]
+        [Display(Name="Your Guess")]
+        public int Guess { get; set; }
+    }
+}
